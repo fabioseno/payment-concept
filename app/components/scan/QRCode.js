86945e8d39ef9657db43cde0a90e5e5d79a8383f
Fábio Akira Yoshida <fabioseno@gmail.com>
2017-10-19 17:58:20 -0200

    Creating a second component

---
+++ app/components/scan/QRCode.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+export class QRCode extends React.Component {
+    render() {
+        return (
+            <View>
+                <Text>oi</Text>
+            </View>
+        );
+    }
+}
